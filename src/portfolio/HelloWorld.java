a1c53b506108a4492fd8090ab47fd79a0f8b71d6
AzerImamaliyev <61497587+AzerImamaliyev@users.noreply.github.com>
2023-04-12 09:49:27 +0400

    Update HelloWorld.java
    Added from GitHub

--- src/portfolio/HelloWorld.java
+++ src/portfolio/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I'm adding some codes to my project");
+		System.out.println("Adding coede from GitHub");
 
 	}
 
