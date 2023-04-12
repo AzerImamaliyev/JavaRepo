c4745bca968c5b018a97d5f8f83f4a0add820e20
Azer Imamaliyev <azer.imamaliyev@yahoo.com>
2023-04-12 09:45:21 +0400

    Added HelloWorld class to the project

---
+++ src/portfolio/HelloWorld.java
@@ -0,0 +1,11 @@
+package portfolio;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("I'm adding some codes to my project");
+
+	}
+
+}
