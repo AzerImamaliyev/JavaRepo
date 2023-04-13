d580fc68bbd86a85d4d6c7669ee0bf200eba0c07
Azer Imamaliyev <azer.imamaliyev@yahoo.com>
2023-04-13 15:01:11 +0400

    HelloWorld modified

--- src/portfolio/HelloWorld.java
+++ src/portfolio/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I'm adding some codes to my project");
-		System.out.println("Adding coede from GitHub");
+		System.out.println("Adding code from GitHub");
 
 	}
 
