281d689aea8100bd6c76b4c08c382653f06566be
Azer Imamaliyev <azer.imamaliyev@yahoo.com>
2023-04-28 13:37:51 +0400

    Guests development finished

--- src/portfolio/Guests.java
+++ src/portfolio/Guests.java
@@ -2,53 +2,41 @@ package portfolio;
 
 import java.util.Scanner;
 
+import oracle.jrockit.jfr.tools.ConCatRepository;
+
 public class Guests {
+
+	static String[] guests = new String[10];
+	static Scanner keyboard = new Scanner(System.in);
 
 	public static void main(String[] args) {
 
-		String[] guests = new String[10];
-		Scanner keyboard = new Scanner(System.in);
-
-		guests[0] = "Jacob";
-		guests[1] = "Edward";
-		guests[2] = "Frank";
+		insertTestNames();
 
 		do {
-			System.out.println("1 - Display all guests");
-			System.out.println("2 - Add guest");
-			System.out.println("3 - Remove guest");
-			System.out.println("4 - Exit");
-			System.out.print("Option: ");
-			int option = keyboard.nextInt();
-			System.out.println();
+			displayGuests();
+
+			displayMenu();
+
+			int option = getOption();
 
 			if (option == 1) {
-				for (String guest : guests) {
-					System.out.println(guest);
-				}
-			} else if (option == 2) {
-				for (int i = 0; i < guests.length; i++) {
-					if (guests[i] == null) {
-						System.out.print("Name: ");
-						String name = keyboard.next();
-						guests[i] = name;
-						break;
 
-					}
-				}
+				addGuest();
+
+			} else if (option == 2) {
+
+				insertGuest();
 
 			} else if (option == 3) {
-				System.out.print("Name: ");
-				String name = keyboard.next();
-				for (int i = 0; i < guests.length; i++) {
 
-					if (guests[i] != null && guests[i].equals(name)) {
-						guests[i] = null;
-					}
-
-				}
+				renameGuest();
 
 			} else if (option == 4) {
+
+				removeGuest();
+
+			} else if (option == 5) {
 				System.out.println("Thank you for choosing our app.");
 				break;
 			}
@@ -57,4 +45,129 @@ public class Guests {
 
 	}
 
+	static void displayGuests() {
+		System.out.println("-----------------------\n       - Guests -");
+		System.out.println("-----------------------");
+
+		boolean isEmpty = true;
+
+		for (int i = 0; i < guests.length; i++) {
+			// System.out.println((i + 1) + ". " + (guests[i] == null ? "--" : guests[i]));
+			if (guests[i] != null) {
+				System.out.println((i + 1) + ". " + guests[i]);
+				isEmpty = false;
+			}
+		}
+
+		if (isEmpty) {
+			System.out.println("Guest list is empty");
+		}
+	}
+
+	static void displayMenu() {
+		System.out.println("-----------------------\n        - Menu -");
+		System.out.println("-----------------------");
+		System.out.println("1 - Add guest");
+		System.out.println("2 - Insert guest");
+		System.out.println("3 - Rename guest");
+		System.out.println("4 - Remove guest");
+		System.out.println("5 - Exit");
+		System.out.println("-----------------------\n");
+	}
+
+	static int getOption() {
+		System.out.print("Option: ");
+		int option = keyboard.nextInt(); // num\n
+		keyboard.nextLine();
+		System.out.println();
+		return option;
+	}
+
+	static void addGuest() {
+		for (int i = 0; i < guests.length; i++) {
+			if (guests[i] == null) {
+				System.out.print("Name: ");
+				String name = keyboard.nextLine();
+				guests[i] = name;
+				break;
+			}
+
+		}
+	}
+
+	static void insertGuest() {
+
+		System.out.print("Number: ");
+		int num = keyboard.nextInt();
+		keyboard.nextLine();
+
+		if (num >= 1 && num <= guests.length && guests[num - 1] != null) {
+
+			System.out.print("Name: ");
+			String newName = keyboard.nextLine();
+
+			for (int i = guests.length - 1; i > num - 1; i--) {
+				guests[i] = guests[i - 1];
+			}
+			guests[num - 1] = newName;
+
+		} else {
+			System.out.println("\nError: There is no guest with that number.");
+		}
+	}
+
+	static void renameGuest() {
+		System.out.print("Number: ");
+		int num = keyboard.nextInt();
+		keyboard.nextLine();
+		System.out.println();
+
+		if (num >= 1 && num <= guests.length && guests[num - 1] != null) {
+
+			System.out.print("Name: ");
+			String newName = keyboard.nextLine();
+
+			guests[num - 1] = newName;
+
+		} else {
+			System.out.println("\nError: There is no guest with that number.");
+		}
+	}
+
+	static void removeGuest() {
+		/*
+		 * System.out.print("Name: "); String name = keyboard.next(); for (int i = 0; i
+		 * < guests.length; i++) {
+		 * 
+		 * if (guests[i] != null && guests[i].equalsIgnoreCase(name)) { guests[i] =
+		 * null; break; } }
+		 */
+
+		System.out.print("Number: ");
+		int num = keyboard.nextInt();
+
+		if (num >= 1 && num <= guests.length && guests[num - 1] != null) {
+			guests[num - 1] = null;
+
+			String[] tempGuests = new String[guests.length];
+			int tempIndex = 0;
+
+			for (int i = 0; i < guests.length; i++) {
+				if (guests[i] != null) {
+					tempGuests[tempIndex] = guests[i];
+					tempIndex++;
+				}
+			}
+			guests = tempGuests;
+		} else {
+			System.out.println("\nError: There is no guest with that number.");
+
+		}
+	}
+
+	static void insertTestNames() {
+		guests[0] = "Jacob Hughes";
+		guests[1] = "Edward Jones";
+		guests[2] = "Frank Morgan";
+	}
 }
